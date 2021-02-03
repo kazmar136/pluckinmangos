bff54eeae00a56fabc0bd0e235340d38eab9a4ef
Olivia <75494110+kazmar136@users.noreply.github.com>
2021-02-02 19:26:49 -0500

    Add files via upload

---
+++ js/mango.js
@@ -0,0 +1,19 @@
+class Mango{
+    constructor(x, y, diametre) {
+        var options = {
+            isStatic:true,
+            'restitution':0.5,
+            'friction':1.0,
+        }
+        this.body = Bodies.circle(x, y, diametre , options);
+        this.diametre=diametre;
+        this.image = loadImage("mango.png");
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        image(this.image, 0, 0, this.diametre);
+        pop();
+      }
+}
